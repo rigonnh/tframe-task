6c9bec59eb2013b4bf0ccfeb9ac9cd01d651023a
rigon <rigonn.h@gmail.com>
2023-07-17 14:44:28 +0200

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tframe-task
+
+# Web App (ReactJS and Spring Boot)
+To start the web app first install all dependecies using the following command `npm install` in the client directory
+and then run the command `npm start` to start ReactJS app
---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <div>
+        <h1>REACT APP</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
